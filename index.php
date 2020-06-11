34caf7b36519aeb26892a1450985433b0b16b05f
hamzat06 <hamzatajanaku@gmail.com>
2020-06-11 20:43:45 +0100

    my first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>BlackTribals</title>
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
+	<!-- Styling for public area -->
+	<link rel="stylesheet" href="static/css/public_styling.css">
+</head>
+<body>
+
+	<!-- container - wraps the whole page -->
+	<div class="container">
+		<!-- navbar -->
+		<div class="navbar">
+			<div class="logo_div">
+				<a href="index.php">Black<span>Tribals</span></a>
+			</div>
+			<ul>
+				<li>
+					<a href="index.html" class="active">Home</a>
+				</li>
+				<li>
+					<a href="#news">News</a>
+				</li>
+				<li>
+					<a href="#contact">Contact</a>
+				</li>
+				<li>
+					<a href="#about">About</a>
+				</li>
+			</ul>
+		</div>
+		<!--// navbar -->
+
+		<!-- Page Content -->
+		<div class="content">
+			<h2 class="content-title">Recent Posts</h2>
+			<hr>
+			<!-- more content still to come here... -->
+		</div>
+		<!--// Page Content -->
+
+		<!-- footer -->
+		<div class="footer">
+			<p>BlackTribalists &copy; <?php echo date('Y'); ?></p>
+		</div>
+		<!--// footer -->
+	</div>
+	<!--// container -->
+	
+</body>
+</html>
